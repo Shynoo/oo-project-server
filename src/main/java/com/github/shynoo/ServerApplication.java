46a24001e9026cc8d42bc32ef628e898afe947c9
Yu <583181285@qq.com>
2016-12-26 19:17:11 +0800

    init

--- src/main/java/com/github/shynoo/ServerApplication.java
+++ src/main/java/com/github/shynoo/ServerApplication.java
@@ -1,11 +1,13 @@
 package com.github.shynoo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ServerApplication{
     
